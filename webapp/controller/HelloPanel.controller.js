8d99c2ecd1fa8d2c97fe7716157c8713b2569723
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-09-05 18:20:16 +0600

    Implementd the doc version of creating a dialog fragment

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ], function(
 	Controller,
-    MessageToast
+    MessageToast,
+    Fragment
 ) {
 	"use strict";
 
@@ -15,6 +17,17 @@ sap.ui.define([
             var sMsg = oBundle.getText("helloMsg", [sRecipient]);
             // show message
             MessageToast.show(sMsg);
+        },
+        onOpenDialog : function () {
+            //create dialog lazily
+            if(!this.pDialog) {
+                this.pDialog = this.loadFragment({
+                    name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                });
+            }
+            this.pDialog.then(function(oDialog) {
+                oDialog.open();
+            });
         }
 	});
 });

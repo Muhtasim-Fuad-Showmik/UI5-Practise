4dda58acb2ef3d90fe27412ce97780103d33a77b
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-09-06 16:05:32 +0600

    Fixed an issued with displaying currecies on the 'Invoices List' page

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,15 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
 ], function (Controller, JSONModel) {
-    "use strict";
+	"use strict";
 
-    return Controller.extend("asp.ui.demo.walkthrough.controller.InvoiceList", {
-        onInit: function () {
-            var oViewModel = new JSONModel({
-                currency: "EUR"
-            });
-            this.getView().setModel(oViewModel, "view");
-        }
-    })
-})
+	return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
+
+		onInit : function () {
+			var oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+		}
+
+	});
+});

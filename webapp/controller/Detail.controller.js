998895c0fe3c751e8ab285a80146ce17eca1fbc7
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-09-08 11:04:16 +0600

    Added a navigation button to the details pages so that users can return to the overview page or last visited page on the app

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent",
+	"sap/ui/core/routing/History"
+], function (Controller, UIComponent, History) {
 	"use strict";
 	return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
 		onInit: function () {
-			var oRouter = this.getOwnerComponent().getRouter();
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
 		},
 		_onObjectMatched: function (oEvent) {
@@ -12,6 +14,18 @@ sap.ui.define([
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
 			});
+		},
+
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
 		}
 	});
 });
